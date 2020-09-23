0956186cc8d067b38935389831bd02ba0bef4aff
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-09-23 11:06:29 +0200

    with functions to bring new state

--- src/shared/rabbitmq/context.go
+++ src/shared/rabbitmq/context.go
@@ -54,21 +54,38 @@ func WithContext(rCtx RabbitMqContext) RabbitMqContext {
 	return c
 }
 
-func (c *context) WithExchange(name, exchangeType string) RabbitMqContext {
-	c.exchangeName = name
-	c.exchangeType = exchangeType
-	return c
+func copy(ctx *context, mutator func(*context)) RabbitMqContext {
+	newCtx := &context{
+		conn:         ctx.conn,
+		channel:      ctx.channel,
+		amqpURI:      ctx.amqpURI,
+		bindingKey:   ctx.bindingKey,
+		exchangeName: ctx.exchangeName,
+		exchangeType: ctx.exchangeType,
+		queueName:    ctx.queueName,
+		done:         ctx.done,
+		handler:      ctx.handler,
+	}
+	mutator(newCtx)
+	return newCtx
+}
 
+func (c *context) WithExchange(name, exchangeType string) RabbitMqContext {
+	return copy(c, func(ctx *context) {
+		ctx.exchangeName = name
+		ctx.exchangeType = exchangeType
+	})
 }
 
 func (c *context) WithQueue(queueName, key string) RabbitMqContext {
-	c.queueName = queueName
-	c.bindingKey = key
-	return c
-
+	return copy(c, func(ctx *context) {
+		ctx.queueName = queueName
+		ctx.bindingKey = key
+	})
 }
 
 func (c *context) WithHandle(handle Handle) RabbitMqContext {
-	c.handler = handle
-	return c
+	return copy(c, func(ctx *context) {
+		ctx.handler = handle
+	})
 }
